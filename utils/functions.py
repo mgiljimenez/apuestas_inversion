5051a9fbc908c92ae5f0fa2bf8986d461a969829
Miguel Gil Jimenez <124299768+mgiljimenez@users.noreply.github.com>
2023-11-15 16:39:50 +0100

    update comentarios scrapp

--- utils/functions.py
+++ utils/functions.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import datetime
 
+"""Importante: Además de este código se hizo webscrapping de https://es.surebet.com/surebets
+Aquí se encontraban oportunidades con una mucha mayor rentabilidad. No se publica aquí el código por política de privacidad de la web.
+Para más detalles puede contactar el desarrollador"""
+
+
 class busqueda_inversion():
     """Clase con la que se realiza el webscrapping y se encuentran las posibles inversiones, su inversion y rentabilidad"""
     def __init__(self):
@@ -16,17 +21,14 @@ class busqueda_inversion():
                 return(False)
         except:
             return(False)
-    # Importamos todo el código DOM de la web de EEUU
+    # Importamos todo el código DOM de la web
     def descargar_datos_partidos(url):
-        """Abrimos la web por la región de EEUU y descargamos el html
-        return: código con las 2 "div" donde se encuentran todos los datos"""
         configuration_driver = webdriver.ChromeOptions()
         configuration_driver.add_argument('--headless')
         driver = webdriver.Chrome(options=configuration_driver)
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         driver.close()
-        #Extraemos los dos bloques div que contienen las dos tablas a explorar y las guardamos en tabla1 y tabla2
         return(soup)
     def optimizacion_streamlit(inversion, mul1, mul2):
         pendiente_media=(mul1-1+(1/(mul2-1)))/2
